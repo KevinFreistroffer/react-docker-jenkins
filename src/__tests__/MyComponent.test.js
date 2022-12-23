4717a2820c7420abee77c22a7fe8c5a0d449ada3
Kevin <kevin.freistroffer@gmail.com>
2022-12-23 15:47:36 -0800

    more tests. render component

--- src/__tests__/MyComponent.test.js
+++ src/__tests__/MyComponent.test.js
@@ -1,7 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import MyComponent from '../MyComponent';
 
 test('renders learn react link', () => {
   render(<MyComponent />);
   expect(screen.getByText('MyComponent functional component')).toBeInTheDocument();
+
+  const textButton = screen.getByRole('button', { name: 'Can\'t click me'});
+  const togglerButton = screen.getByRole('button', { name: 'Toggler'})
+
+  expect(textButton).toBeDisabled();
+  userEvent.click(togglerButton);
+  expect(textButton).not.toBeDisabled();
 });
